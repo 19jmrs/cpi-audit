2949426d11a78a7afa6c3e0ed1a557c1921f7d86
Jorge <jorgesa.developer@gmail.com>
2025-02-20 20:56:35 +0000

    connection to api

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/19jmrs/cpi-audit
 
 go 1.22.3
+
+require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/oauth2 v0.26.0
+)
--- main.go
+++ main.go
@@ -1,8 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
 
-func main(){
-	fmt.Println("Hello World");
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+type IntegrationPackages struct {
+	Id string `json:Id`
+	Description string `json:Description`
+	Version string `json:Version`
+	Vendor string `json:Vendor`
+	ShortText string `json:ShortText`
+	
 }
 
+func main() {
+    // Load environment variables from .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    // Example: Get environment variables
+    TokenUrl  := os.Getenv("tokenurl")
+    ClientID := os.Getenv("clientid")
+    ClientSecret := os.Getenv("clientsecret")
+	TenantUrl := os.Getenv("tenanturl")
+	
+
+	config := clientcredentials.Config{
+		ClientID: ClientID,
+		ClientSecret: ClientSecret,
+		TokenURL: TokenUrl,
+	}
+
+	ctx := context.Background()
+
+	_,err = config.Token(ctx)
+	if err != nil{
+		log.Fatal("Error getting token: ", err)
+	}
+
+	client := config.Client(ctx)
+
+	resp, err := client.Get(TenantUrl+"IntegrationPackages")
+
+	if err != nil {
+		log.Fatal("Error getting Packages: ", err)
+	}
+
+	fmt.Printf("Packages: ", resp)
+	
+}
